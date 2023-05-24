a2d72701d7b49de5f1f6f2d9448521ac1f1daf62
nameless-akash <akshsgh315@gmail.com>
2023-05-24 23:11:05 +0530

    main file

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+from demo import make_animation
+import imageio
+from skimage.transform import resize
+from IPython.display import HTML
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pathToModel = "model.pth.tar"
+pathToImage = "1.jpg"
+pathToVideo = "1.mp4"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from demo import load_checkpoints
+generator, kp_detector = load_checkpoints(config_path='vox-256.yaml',
+                            checkpoint_path='pathToModel')
+
+def display(source, driving, generated=None):
+    fig = plt.figure(figsize=(8 + 4 * (generated is not None), 6))
+
+    ims = []
+    for i in range(len(driving)):
+        cols = [source]
+        cols.append(driving[i])
+        if generated is not None:
+            cols.append(generated[i])
+        im = plt.imshow(np.concatenate(cols, axis=1), animated=True)
+        plt.axis('off')
+        ims.append([im])
+
+    ani = animation.ArtistAnimation(fig, ims, interval=50, repeat_delay=1000)
+    plt.close()
+    return ani
+
+source_image = imageio.imread(pathToImage)
+driving_video = imageio.mimread(pathToVideo, memtest=False)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#Resize image and video to 256x256
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+source_image = resize(source_image, (256, 256))[..., :3]
+driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+
+predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=True,
+                             adapt_movement_scale=True)
+
+HTML(display(source_image, driving_video, predictions).to_html5_video())
